878c54069c7e835b2a85f3479621b42101260b6f
mareadelmar <mar.ortega.v@gmail.com>
2021-12-29 11:33:41 -0300

    Add sidebar's options and icons

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import "./Sidebar.css";
+import SidebarOption from "../SidebarOption";
 
 const Sidebar = () => {
 	return (
 		<section className='sidebar-container'>
 			<p>SIDEBAR</p>
+			<SidebarOption title='Home' icon='mdi-light:home' />
+			<SidebarOption title='Search' icon='mdi-light:magnify' />
+			<SidebarOption title='Library' icon='mdi-light:music' />
 		</section>
 	);
 };
---
+++ src/components/SidebarOption/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./SidebarOption.css";
+import { Icon } from "@iconify/react";
+
+const SidebarOption = ({ title, icon }) => {
+	return (
+		<div className='sidebarOption-container'>
+			{icon && <Icon icon={icon} className='sidebarOption-icon' />}
+			{icon ? <h4>{title}</h4> : <p>{title}</p>}
+		</div>
+	);
+};
+
+export default SidebarOption;
