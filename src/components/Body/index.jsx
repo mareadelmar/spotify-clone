7dac0fdbf29f028f3753e3ed2a5f257e315e16af
mareadelmar <mar.ortega.v@gmail.com>
2021-12-30 18:11:43 -0300

    Create body's header structure

--- src/components/Body/index.jsx
+++ src/components/Body/index.jsx
@@ -1,10 +1,30 @@
 import React from "react";
 import "./Body.css";
+import BodyHeader from "../BodyHeader";
 
 const Body = () => {
 	return (
 		<section className='body-container'>
-			<p>BODY</p>
+			<BodyHeader />
+
+			<div className='body-discover flex'>
+				<img
+					src='https://i.scdn.co/image/ab67616d0000b2739e569e2250c16f6089a61cca'
+					alt=''
+					className='discover-img'
+				/>
+				<div className='discover-text flex flex-column'>
+					<p>
+						<strong>PLAYLISTS</strong>
+					</p>
+					<h3>Discover Weekly</h3>
+					<p>
+						Lorem ipsum dolor sit amet consectetur, adipisicing
+						elit. Ad voluptatibus suscipit fuga nesciunt. Possimus
+						tenetur reiciendis nisi.
+					</p>
+				</div>
+			</div>
 		</section>
 	);
 };
---
+++ src/components/BodyHeader/index.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./BodyHeader.css";
+import { useUserData } from "../../context/UserContext";
+import { Avatar } from "@material-ui/core";
+
+const BodyHeader = () => {
+	const [{ spotify, user }] = useUserData();
+
+	return (
+		<header className='header-container'>
+			<div className='header-avatar flex align-center justify-center'>
+				<Avatar
+					src={user?.images[0]?.url}
+					alt={user?.display_name}
+					className='avatar'
+				/>
+				<p>
+					<strong>{user?.display_name}</strong>
+				</p>
+			</div>
+		</header>
+	);
+};
+
+export default BodyHeader;
