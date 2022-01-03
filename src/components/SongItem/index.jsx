2ebcf19f262715166f6e0e75d08c5be5c721981f
mareadelmar <mar.ortega.v@gmail.com>
2022-01-03 14:14:33 -0300

    Styles fix song item and body

--- src/components/SongItem/index.jsx
+++ src/components/SongItem/index.jsx
@@ -4,8 +4,12 @@ import "./SongItem.css";
 const SongItem = ({ track }) => {
 	return (
 		<div className='song-container flex align-center'>
-			<img src={track.album?.images[0]?.url} alt={track.album.name} />
-			<div>
+			<img
+				className='song-img'
+				src={track.album?.images[0]?.url}
+				alt={track.album.name}
+			/>
+			<div className='song-details'>
 				<h3>{track.name}</h3>
 				<p>
 					{track.artists.map(item => item.name).join(", ")} -{" "}
